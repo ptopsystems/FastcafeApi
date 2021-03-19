ea8c937245ae6fab700d15ed743f9a97d2f48fca
uiyoon <nauy@toz.co.kr>
2021-03-19 15:33:35 +0900

    문의 필드면 변경 - 20210319

--- src/main/java/com/rest/api/entity/fastcafe_admin/Board.java
+++ src/main/java/com/rest/api/entity/fastcafe_admin/Board.java
@@ -1,7 +1,7 @@
 package com.rest.api.entity.fastcafe_admin;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -23,21 +23,23 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "branch_id")
     private int branchId;
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "admin_id")
     private int adminId;
     private String title;
-    private String content;
+    private String contents;
     private String accidentTime;
     private String attachImgUrl;
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "answer_member_id")
     private int answerMemberId;
     private String answer;
     private String answerFileUrl;
+    private String answerFileName;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Timestamp answerdate;
     private String stat;
     @CreationTimestamp
