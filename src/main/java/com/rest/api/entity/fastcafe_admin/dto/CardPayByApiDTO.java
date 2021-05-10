83722d20ec064c8fdc856ac6b4ee478b0fe0fae9
uiyoon <nauy@toz.co.kr>
2021-05-10 16:08:42 +0900

    파라미터명 변경 regdate -> paydate

--- src/main/java/com/rest/api/entity/fastcafe_admin/dto/CardPayByApiDTO.java
+++ src/main/java/com/rest/api/entity/fastcafe_admin/dto/CardPayByApiDTO.java
@@ -13,11 +13,11 @@ import java.time.format.DateTimeFormatter;
 public class CardPayByApiDTO {
     private int payMoney;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime regdate;
+    private LocalDateTime paydate;
 
     public CardPayByApiDTO(CardPayByApi cardPayByApi) {
         this.payMoney = cardPayByApi.getAppAmt();
-        this.regdate = LocalDateTime.parse(cardPayByApi.getTransDate() + " " + cardPayByApi.getTransTime(), DateTimeFormatter.ofPattern("yyyy-MM-dd HHmmss"));
+        this.paydate = LocalDateTime.parse(cardPayByApi.getTransDate() + " " + cardPayByApi.getTransTime(), DateTimeFormatter.ofPattern("yyyy-MM-dd HHmmss"));
 
     }
 }
